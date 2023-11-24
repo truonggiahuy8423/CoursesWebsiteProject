82cc57bec626d1e8207deb42b619813d892693b5
sunsun1102 <sonhoang880@gmail.com>
2023-11-24 10:33:54 +0700

    new update

--- public/assets/script.js
+++ public/assets/script.js
@@ -45,53 +45,50 @@
 //   });
 // }
 
-function toast({
-  title = '',
-  message = '',
-  type = 'info',
-  duration = 3000
-}) {
-  const main = document.getElementById('toast');
-  if (main) {
-    const toast = documen.createElement('div');
 
-    //Auto remove toast
+// Toast function
+function toast({ title = "", message = "", type = "info", duration = 3000 }) {
+  const main = document.getElementById("toast");
+  if (main) {
+    const toast = document.createElement("div");
+
+    // Auto remove toast
     const autoRemoveId = setTimeout(function () {
       main.removeChild(toast);
     }, duration + 1000);
 
-    //Remove toast when clicked
+    // Remove toast when clicked
     toast.onclick = function (e) {
-      if (e.target.closest('.toast__close')) {
+      if (e.target.closest(".toast__close")) {
         main.removeChild(toast);
         clearTimeout(autoRemoveId);
       }
-    }
+    };
 
     const icons = {
-      success: 'fa-solid fa-circle-check',
-      info: 'fa-solid fa-circle-info',
-      warning: 'fa-solid fa-circle-exclamation',
-      error: 'fa-solid fa-circle-exclamation',
+      success: "fa-solid fa-circle-check",
+      info: "fa-solid fa-circle-info",
+      warning: "fa-solid fa-circle-exclamation",
+      error: "fa-solid fa-circle-exclamation"
     };
     const icon = icons[type];
     const delay = (duration / 1000).toFixed(2);
 
-    toast.classList.add('toast', `toast--${type}`);
-    toast.style.animation = `slideInLeft case .3s, fadeOut linear 1s ${delay}s forwards`;
+    toast.classList.add("toast", `toast--${type}`);
+    toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
 
     toast.innerHTML = `
-          <div class="toast__icon">
-              <i class="fa-solid fa-circle-check"></i>
-          </div>
-          <div class="toast__body">
-              <h3 class="toast__title">${title}</h3>
-              <p class="toast__msg">${message}</p>
-          </div>
-          <div class="toast__close">
-              <i class="fa-solid fa-circle-xmark"></i>
-          </div>
-      `;
+      <div class="toast__icon">
+          <i class="${icon}"></i>
+      </div>
+      <div class="toast__body">
+          <h3 class="toast__title">${title}</h3>
+          <p class="toast__msg">${message}</p>
+      </div>
+      <div class="toast__close">
+          <i class="fa-solid fa-circle-xmark"></i>
+      </div>
+    `;
     main.appendChild(toast);
   }
 }
