9faf3190f8ce10645b49032c73893e8fd5af9b54
sunsun1102 <sonhoang880@gmail.com>
2023-12-01 09:47:24 +0700

    1/12

--- app/Views/Admin/ViewCell/InsertStudentForm.php
+++ app/Views/Admin/ViewCell/InsertStudentForm.php
@@ -10,25 +10,26 @@
         <!-- Your form goes here -->
         <form>
           <div class="mb-3">
-            <label for="hoTen" class="form-label">Họ Tên</label>
-            <input type="text" class="form-control" id="hoTen">
+            <label for="hoTen" class="form-label name">Họ Tên</label> <span id="error_name" class="text-danger ms-3"></span>
+            <input type="text" class="form-control" id="hoTen" placeholder="Họ tên">
           </div>
           <div class="mb-3">
-            <label for="ngaySinh" class="form-label">Ngày Sinh</label>
+            <label for="ngaySinh" class="form-label dob">Ngày Sinh</label> <span id="error_dob" class="text-danger ms-3"></span>
             <input type="date" class="form-control" id="ngaySinh">
           </div>
           <div class="mb-3">
-            <label for="gioiTinh" class="form-label">Giới Tính</label>
+            <label for="gioiTinh" class="form-label gender">Giới Tính</label> <span id="error_gender" class="text-danger ms-3"></span>
             <select class="form-select" id="gioiTinh">
+              <option value="" disabled selected>Chọn giới tính</option>
               <option value="1">Nam</option>
               <option value="2">Nữ</option>
             </select>
           </div>
           <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email">
+            <label for="email" class="form-label mail">Email</label> <span id="error_email" class="text-danger ms-3"></span>
+            <input type="email" class="form-control" id="email" placeholder="Email">
           </div>
-          <button type="button" class="btn btn-primary" onclick="saveStudent()">Lưu</button>
+          <button type="button" class="btn btn-primary save-student-btn" onclick="saveStudent()">Lưu</button>
         </form>
       </div>
     </div>
@@ -38,62 +39,112 @@
 <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha384-UG8ao2jwOWB7/oDdObZc6ItJmwUkR/PfMyt9Qs5AwX7PsnYn1CRKCTWyncPTWvaS" crossorigin="anonymous"></script>
 
 <script>
-  function saveStudent() {
-      // Get values from the form
-    var hoTen = document.getElementById('hoTen').value;
-    var ngaySinh = document.getElementById('ngaySinh').value;
-    var gioiTinh = document.getElementById('gioiTinh').value;
-    var email = document.getElementById('email').value;
-  
-    // Perform validation if needed
-    $.ajax({
-      url: '<?php echo base_url(); ?>/Admin/StudentsController/insertStudent',
-      method: 'GET',
-      //dataType: 'json',
-      contentType: "json",
-      data: JSON.stringify({
-        hoTen: hoTen,
-        ngaySinh: ngaySinh,
-        gioiTinh: gioiTinh,
-        email: email
-      }),
-      success: function(response) {
-        if (response.success) {
-            
-            $('#InsertStudentForm').modal('hide');
-            
-            // Display success toast
-            toast({
-                title: "Thành công!",
-                message: "Thêm học viên thành công",
-                type: "success",
-                duration: 3000
-            });
+  $(document).ready(function () {
+    $(document).on('click', '.save-student-btn', function() {
+      var hoTen = document.getElementById('hoTen').value;
+      var ngaySinh = document.getElementById('ngaySinh').value;
+      var gioiTinh = document.getElementById('gioiTinh').value;
+      var email = document.getElementById('email').value;
 
-            
-        } else {
-            
-            alert('Failed to save student. Please try again.');
-        }
-      },
-      error: function(xhr, status, error) {
+      if ($.trim($('.name').val().length == 0)) {
+        error_name = 'Vui lòng điền họ tên';
+        $('#error_name').text(error_name);
+      } else {
+        error_name = '';
+        $('#error_name').text(error_name);
+      }
+      
+      if ($.trim($('.dob').val().length == 0)) {
+        error_dob = 'Vui lòng chọn ngày sinh';
+        $('#error_dob').text(error_dob);
+      } else {
+        error_dob = '';
+        $('#error_dob').text(error_dob);
+      }
 
-          toast({
-              title: "Thất bại!",
-              message: "không thể thêm học viên",
-              type: "error",
-              duration: 3000
-          });
+      if ($.trim($('.gender').val().length == 0)) {
+        error_gender = 'Vui lòng chọn giới tính';
+        $('#error_gender').text(error_gender);
+      } else {
+        error_gender = '';
+        $('#error_gender').text(error_gender);
+      }
 
-          console.error('Error:', status, error);
+      if ($.trim($('.mail').val().length == 0)) {
+        error_email = 'Vui lòng điền Email';
+        $('#error_email').text(error_email);
+      } else {
+        error_email = '';
+        $('#error_email').text(error_email);
+      }
+
+      if (error_name != '' || error_dob != '' || error_gender = '' || error_email != '') {
+        return false;
+      }
+      else{
+        $.ajax({
+          method: "POST",
+          url: "/"
+        })
       }
     });
-  }
+  });
+  // function saveStudent() {
+  //     // Get values from the form
+  //   var hoTen = document.getElementById('hoTen').value;
+  //   var ngaySinh = document.getElementById('ngaySinh').value;
+  //   var gioiTinh = document.getElementById('gioiTinh').value;
+  //   var email = document.getElementById('email').value;
+  
+  //   // Perform validation if needed
+  //   $.ajax({
+  //     url: '<?php echo base_url(); ?>/Admin/StudentsController/insertStudent',
+  //     method: 'GET',
+  //     //dataType: 'json',
+  //     contentType: "json",
+  //     data: JSON.stringify({
+  //       hoTen: hoTen,
+  //       ngaySinh: ngaySinh,
+  //       gioiTinh: gioiTinh,
+  //       email: email
+  //     }),
+  //     success: function(response) {
+  //       if (response.success) {
+            
+  //           $('#InsertStudentForm').modal('hide');
+            
+  //           // Display success toast
+  //           toast({
+  //               title: "Thành công!",
+  //               message: "Thêm học viên thành công",
+  //               type: "success",
+  //               duration: 3000
+  //           });
+
+            
+  //       } else {
+            
+  //           alert('Failed to save student. Please try again.');
+  //       }
+  //     },
+  //     error: function(xhr, status, error) {
+
+  //         toast({
+  //             title: "Thất bại!",
+  //             message: "không thể thêm học viên",
+  //             type: "error",
+  //             duration: 3000
+  //         });
+
+  //         console.error('Error:', status, error);
+  //     }
+  //   });
+  // }
 </script>
 
 
 <!-- Modal Sửa -->
-<div class="modal fade" id="SuaStudentForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- <div class="modal fade" id="SuaStudentForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -101,7 +152,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <!-- Form Sửa -->
+
                 <form>
                     <div class="mb-3">
                         <label for="hoTenSua" class="form-label">Họ Tên</label>
@@ -158,5 +209,5 @@
 
         $('#SuaStudentForm').modal('hide');
     }
-</script>
+</script> -->
 
