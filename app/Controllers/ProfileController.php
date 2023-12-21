7e97929a49cf66322ded3b63363adb4be412d1c9
truonggiahuy8423 <zhuy8423@gmail.com>
2023-12-21 22:20:31 +0700

    ok

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ use App\Models\UserModel;
 use App\Models\GiangVienModel;
 use App\Models\hoc_vien_tham_giaModel;
 use App\Models\HocVienModel;
+use App\Models\TinNhanRiengModel;
 use App\Models\phan_cong_giang_vienModel;
 use DateTime;
 class ProfileController extends BaseController
@@ -58,6 +59,7 @@ class ProfileController extends BaseController
             $data['user'] = $lecturersModel->getGiangVienById($teacherID);
             $data["avatar_data"] = ((new UserModel())->getUserByLecturerId($data['user']->id_giang_vien))->anh_dai_dien;
             $data['attend'] = $phancong->getPhanCongByIDGiangVien($teacherID);
+            $navbar_data['chatBox'] = $this->getChatBox();
             $data['navbar'] = view('Admin\ViewCell\NavBar', $navbar_data);
             $data['role'] = true;
             return view('ProfilePage', $data);
@@ -72,9 +74,43 @@ class ProfileController extends BaseController
             $data['user'] = $studentModel->getHocVienById($id_hoc_vien);
             $data["avatar_data"] = ((new UserModel())->getUserByStudentId($data['user']->id_hoc_vien))->anh_dai_dien;
             $data['attend'] = $phancong->getHocVienThamGiaByHocVienID($id_hoc_vien);
+            $navbar_data['chatBox'] = $this->getChatBox();
             $data['navbar'] = view('Admin\ViewCell\NavBar', $navbar_data);
             $data['role'] = false;
             return view('ProfilePage', $data);
         }
+    }
+
+    public function getChatBox(){
+        $tinNhanRiengModel = new TinNhanRiengModel();
+        $chat_box = $tinNhanRiengModel->queryDatabase(
+            'SELECT DISTINCT user_nhan
+            FROM tin_nhan_rieng
+            WHERE user_gui = ' . session()->get("id_user"));
+
+        for($i = 0; $i < count($chat_box); $i++){
+            $user_nhan = strval($chat_box[$i]["user_nhan"]);
+            $chat_box[$i]['lastestTime'] = $tinNhanRiengModel->queryDatabase(
+                'SELECT thoi_gian, anh
+                FROM tin_nhan_rieng
+                WHERE user_gui IN ('.$user_nhan .','.session()->get("id_user").')
+                AND user_nhan IN ('.$user_nhan .','.session()->get("id_user").')
+                ORDER BY thoi_gian DESC
+                LIMIT 1');
+            $chat_box[$i]['hoTen'] = $tinNhanRiengModel->queryDatabase(
+                'SELECT
+                    CASE
+                        WHEN u.id_giang_vien IS NOT NULL THEN gv.ho_ten
+                        WHEN u.id_ad IS NOT NULL THEN ad.ho_ten
+                        WHEN u.id_hoc_vien IS NOT NULL THEN hv.ho_ten
+                    END AS ho_ten
+                FROM
+                    users u
+                LEFT JOIN giang_vien gv ON u.id_giang_vien = gv.id_giang_vien
+                LEFT JOIN ad ON u.id_ad = ad.id_ad
+                LEFT JOIN hoc_vien hv ON u.id_hoc_vien = hv.id_hoc_vien
+                WHERE u.id_user = ' . $user_nhan);
+        }
+        return $chat_box;
     }
 }
