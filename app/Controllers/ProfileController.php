6b1dec2e7af499511a907c8f77e44ace118c7a0b
truonggiahuy8423 <zhuy8423@gmail.com>
2023-12-21 22:41:23 +0700

    Revert "okkkk"
    This reverts commit 7bfb0aa0c3e1b17a2ab083955db179b69dff3b17.

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -5,7 +5,6 @@ use App\Models\UserModel;
 use App\Models\GiangVienModel;
 use App\Models\hoc_vien_tham_giaModel;
 use App\Models\HocVienModel;
-use App\Models\TinNhanRiengModel;
 use App\Models\phan_cong_giang_vienModel;
 use DateTime;
 class ProfileController extends BaseController
@@ -77,38 +76,5 @@ class ProfileController extends BaseController
             $data['role'] = false;
             return view('ProfilePage', $data);
         }
-    }
-
-    public function getChatBox(){
-        $tinNhanRiengModel = new TinNhanRiengModel();
-        $chat_box = $tinNhanRiengModel->queryDatabase(
-            'SELECT DISTINCT user_nhan
-            FROM tin_nhan_rieng
-            WHERE user_gui = ' . session()->get("id_user"));
-
-        for($i = 0; $i < count($chat_box); $i++){
-            $user_nhan = strval($chat_box[$i]["user_nhan"]);
-            $chat_box[$i]['lastestTime'] = $tinNhanRiengModel->queryDatabase(
-                'SELECT thoi_gian, anh
-                FROM tin_nhan_rieng
-                WHERE user_gui IN ('.$user_nhan .','.session()->get("id_user").')
-                AND user_nhan IN ('.$user_nhan .','.session()->get("id_user").')
-                ORDER BY thoi_gian DESC
-                LIMIT 1');
-            $chat_box[$i]['hoTen'] = $tinNhanRiengModel->queryDatabase(
-                'SELECT
-                    CASE
-                        WHEN u.id_giang_vien IS NOT NULL THEN gv.ho_ten
-                        WHEN u.id_ad IS NOT NULL THEN ad.ho_ten
-                        WHEN u.id_hoc_vien IS NOT NULL THEN hv.ho_ten
-                    END AS ho_ten
-                FROM
-                    users u
-                LEFT JOIN giang_vien gv ON u.id_giang_vien = gv.id_giang_vien
-                LEFT JOIN ad ON u.id_ad = ad.id_ad
-                LEFT JOIN hoc_vien hv ON u.id_hoc_vien = hv.id_hoc_vien
-                WHERE u.id_user = ' . $user_nhan);
-        }
-        return $chat_box;
     }
 }
--- app/Views/Admin/ViewCell/NavBar.php
+++ app/Views/Admin/ViewCell/NavBar.php
@@ -1,3 +1,5 @@
+
+
 <div class="top-nav">
         <a class="top-nav__logo" href="<?php base_url() ?>/courses">
             <img src="<?php base_url() ?>/assets/img/small_logo_course.png" alt="">
@@ -15,74 +17,8 @@
                 <img src="<?php base_url() ?>/assets/img/message_icon.png" alt="">
             </button>
             <div style="position: relative;">
-                <div class="message-pop-up card p-2">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <h4>Tin nhắn</h4>
-                        <button class="btn addChatBoxBtn"><i class="fa-solid fa-plus" style="color: #000000;"></i></button>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <?php
-                            // echo var_dump($chatBox);
-                            for($i = 0; $i < count($chatBox); $i++){
-                                $avaImg = checkImg($chatBox[$i]["lastestTime"][0]["anh"]);
-                                echo "
-                                <div class='chatBox col-12 mb-2' chatBoxID='{$chatBox[$i]["user_nhan"]}'>
-                                    <div class='card'>
-                                        <div class='card-body p-3'>
-                                            <div class='row'>
-                                                <div class='col-4 d-flex justify-content-center align-items-center ps-3 pe-0'>
-                                                    <img class='logo img-responsive img-thumbnail rounded-circle'  src='{$avaImg}' alt='avatar'>
-                                                </div>
-                                                <div class='col-8 d-flex justify-content-center align-items-start flex-column p-2'>
-                                                    <h5 class='card-title' style='font-size: 15px;'>{$chatBox[$i]["hoTen"][0]["ho_ten"]}</h5>
-                                                    <p class='card-subtitle text-muted'>{$chatBox[$i]["lastestTime"][0]["thoi_gian"]}</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>";
-                            }
+                <div class="message-pop-up">
 
-                            function checkImg($avaImg){
-                                if ($avaImg != null) {
-                                    $base64Image = base64_encode($avaImg);
-                                    return "data:image/png;base64," . $base64Image;
-                                }  else {
-                                    return base_url()."assets/img/avatar_blank.jpg";
-                                }
-                            }
-                        ?>
-                        <div class="col-12 mb-2">
-                            <div class="card">
-                                <div class="card-body p-3">
-                                    <div class="row">
-                                        <div class="col-4 d-flex justify-content-center align-items-center ps-3 pe-0">
-                                            <img class="logo img-responsive img-thumbnail rounded-circle"  src="<?php base_url() ?>/assets/img/avatar_blank.jpg" alt="avatar">
-                                        </div>
-                                        <div class="col-8 d-flex justify-content-center align-items-start flex-column p-2">
-                                            <h5 class="card-title" style="font-size: 15px;">Nguyễn Duy Khánh</h5>
-                                            <p class="card-subtitle text-muted">3 mins ago</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div style="position: relative;">
-                <div class="inbox card p-2" style="background-color: #fff">
-                    <!-- inbox_header -->
-                    
-                    <!-- inbox_body -->
-
-                    <div class="inbox__footer mt-3">
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control inputChat" placeholder="Write a message" aria-describedby="button-addon2">
-                            <button class="btn btn-outline-secondary sendBtn" type="button" id="button-addon2">Send</button>
-                        </div>
-                    </div>
                 </div>
             </div>
             <div class="top-nav__drop-down">
@@ -137,15 +73,6 @@
                         $('.message-pop-up').css('visibility', 'hidden');
                         $('.message-pop-up').css('opacity', '0');
                         $('.top-nav__message').css('box-shadow', 'none');
-                        $('.inbox').css('opacity', '0');
-                        $('.inbox').css('right', '-300px');
-                        $('.inbox__header').remove();
-                        $('.hr1').remove();
-                        $('.inbox__body').remove();
-                        isInboxOpen = false;
-                        isScrollTop = false;
-                        currentScrollTop;
-                        previousInputChat = '';
                     } else if (notification_popup_state) {
                         notification_popup_state = !notification_popup_state;
                         $('.notification-pop-up').css('visibility', 'hidden');
@@ -182,20 +109,13 @@
                     }
                     message_popup_state = !message_popup_state;
                     $('.message-pop-up').css('visibility', 'visible');
-                    $('.message-pop-up').css('right', '0');
                     $('.message-pop-up').css('opacity', '1');
                     $('.top-nav__message').css('box-shadow', '0 0px 4px rgba(255, 255, 255, 1)');
                 } else {
                     message_popup_state = !message_popup_state;
+                    $('.message-pop-up').css('visibility', 'hidden');
                     $('.message-pop-up').css('opacity', '0');
-                    $('.message-pop-up').css('right', '-300px');
-                    $('.inbox').css('opacity', '0');
-                    $('.inbox').css('right', '-300px');
-                    $('.inbox__header').remove();
-                    $('.hr1').remove();
-                    $('.inbox__body').remove();
                     $('.top-nav__message').css('box-shadow', 'none');
-                    isInboxOpen = false;
                 }
             })
             var notification_popup_state = false;
@@ -206,15 +126,6 @@
                         $('.message-pop-up').css('visibility', 'hidden');
                         $('.message-pop-up').css('opacity', '0');
                         $('.top-nav__message').css('box-shadow', 'none');
-                        $('.inbox').css('opacity', '0');
-                        $('.inbox').css('right', '-300px');
-                        $('.inbox__header').remove();
-                        $('.hr1').remove();
-                        $('.inbox__body').remove();
-                        isInboxOpen = false;
-                        isScrollTop = false;
-                        currentScrollTop;
-                        previousInputChat = '';
                     } else if (profile_popup_state) {
                         profile_popup_state = !profile_popup_state;
                         $('.top-nav__drop-down #drop-down-icon').css('transform', 'rotate(0deg)');
@@ -244,142 +155,6 @@
 
                 }
             });
-            
-            $('.addChatBoxBtn').click(function(){
-                $.ajax({
-                    url: '<?php echo base_url(); ?>/TinNhanController/getInsertChatBoxForm',
-                    method: 'GET',
-                    success: function(response) {
-                        $('body').append(response);
-
-                        $(`.insert-chatbox-form__cancel-btn`).click(function() {
-                            $('.form-container').remove();
-                        });
-
-                        $('.sendBtn_newChat').click(function() {
-                                var noi_dung_tin_nhan = $('.inputNewChat').val();
-                                var chatBoxID = $('.addNewChatBox:checked').val();
-                                console.log(chatBoxID);
-                                $('.inputNewChat').val('');
-                                var obj = {
-                                    noi_dung: noi_dung_tin_nhan,
-                                    user_nhan: chatBoxID,
-                                }
-                                var jsonData = JSON.stringify(obj);
-
-                                $.ajax({
-                                    url: '<?php echo base_url(); ?>/TinNhanController/sendTinNhanRieng',
-                                    method: 'POST',
-                                    contentType: 'application/json',
-                                    data: jsonData,
-                                    success: function(response) {
-                                        
-                                    },
-                                    error: function(xhr, status, error) {
-                                        console.error('Error:', status, error);
-                                    }
-                                });
-                            });
-                    }
-                })
-            })
-
         })
-        var isInboxOpen = false;
-        var isScrollTop = false;
-        var currentScrollTop;
-        var previousInputChat = '';
-        $(document).on('click', '.offInboxBtn', function(){
-            $('.inbox').css('opacity', '0');
-            $('.inbox').css('right', '-300px');
-            $('.inbox__header').remove();
-            $('.hr1').remove();
-            $('.inbox__body').remove();
-            isInboxOpen = false;
-            isScrollTop = false;
-            currentScrollTop;
-            previousInputChat = '';
-        })
-        $(document).on('click', '.chatBox', function(){
-                $('.inbox').css('visibility', 'visible');
-                $('.inbox').css('right', '0');
-                $('.inbox').css('opacity', '1');
-                var chatboxID = $(this).attr('chatBoxID');
-                console.log(chatboxID);
-                if(!isInboxOpen){
-                    isInboxOpen = true;
-                    (function update() {
-                        $.ajax({
-                            url: '<?php echo base_url(); ?>/TinNhanController/getInBox',
-                            method: 'GET',
-                            data: {
-                                chatboxID: chatboxID
-                            },
-                            success: function(response) {
-                                $('.inbox__header').remove();
-                                $('.hr1').remove();
-                                $('.inbox__body').remove();
-
-                                $('.inbox__footer').before(response);
-                                console.log(response);
-                                if(!isScrollTop){
-                                    currentScrollTop = $('.inboxContent').height();
-                                }
-                                $('.inbox__body').scroll(function(){
-                                    isScrollTop = true;
-                                    currentScrollTop = $(this).scrollTop();
-                                })
-                                $('.inbox__body').scrollTop(currentScrollTop);
-
-
-                                console.log("inbox refresh");
-                                $('.sendBtn').click(function() {
-                                    var noi_dung_tin_nhan = $('.inputChat').val();
-                                    $('.inputChat').val('');
-                                    var obj = {
-                                        noi_dung: noi_dung_tin_nhan,
-                                        user_nhan: chatboxID,
-                                    }
-                                    var jsonData = JSON.stringify(obj);
-                                    // console.log(noi_dung_tin_nhan);
-                                    // console.log(jsonData);
-                                    if(noi_dung_tin_nhan.length != 0){
-                                        $.ajax({
-                                            url: '<?php echo base_url(); ?>/TinNhanController/sendTinNhanRieng',
-                                            method: 'POST',
-                                            contentType: 'application/json',
-                                            data: jsonData,
-                                            success: function(response) {
-                                                if(response.state) {
-                                                    $('.inboxContent').append(`
-                                                    <div class="col-7 offset-5 mb-1">
-                                                        <div class="card">
-                                                            <p class="p-1">${obj.noi_dung}</p>
-                                                        </div>
-                                                    </div>`);
-                                                }
-                                                $('.inbox__body').scrollTop($('.inboxContent').height());
-                                            },
-                                            error: function(xhr, status, error) {
-                                                console.error('Error:', status, error);
-                                            }
-                                        });
-                                    }
-                                });
-                            },
-                            error: function(xhr, status, error) {
-                                console.error(xhr);
-                                console.error('Lỗi yêu cầu:', status, error);
-                            }
-                        }).then(function() {           // on completion, restart
-                            if(isInboxOpen) setTimeout(update, 1000);  // function refers to itself
-                        });
-                    })();  
-                }else{
-                    isInboxOpen = false;
-                    isScrollTop = false;
-                }
-                
-            });
     </script>
 </body>
